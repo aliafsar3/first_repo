2e859fd90d31a4c8860819b38d4afa5e0f68f248
afsar-ali-786 <aliafsar861@gmai.com>
2021-06-20 10:20:11 +0530

    update1

--- admin/update-package.php
+++ admin/update-package.php
@@ -1,12 +1,6 @@
 <?php
-session_start();
-error_reporting(0);
 include('includes/config.php');
-if(strlen($_SESSION['alogin'])==0)
-	{	
-header('location:index.php');
-}
-else{
+
 $pid=intval($_GET['pid']);	
 if(isset($_POST['submit']))
 {
@@ -17,26 +11,28 @@ $pprice=$_POST['packageprice'];
 $pfeatures=$_POST['packagefeatures'];
 $pdetails=$_POST['packagedetails'];	
 $pimage=$_FILES["packageimage"]["name"];
-$sql="update TblTourPackages set PackageName=:pname,PackageType=:ptype,PackageLocation=:plocation,PackagePrice=:pprice,PackageFetures=:pfeatures,PackageDetails=:pdetails where PackageId=:pid";
-$query = $dbh->prepare($sql);
-$query->bindParam(':pname',$pname,PDO::PARAM_STR);
-$query->bindParam(':ptype',$ptype,PDO::PARAM_STR);
-$query->bindParam(':plocation',$plocation,PDO::PARAM_STR);
-$query->bindParam(':pprice',$pprice,PDO::PARAM_STR);
-$query->bindParam(':pfeatures',$pfeatures,PDO::PARAM_STR);
-$query->bindParam(':pdetails',$pdetails,PDO::PARAM_STR);
-$query->bindParam(':pid',$pid,PDO::PARAM_STR);
-$query->execute();
-$msg="Package Updated Successfully";
-}
 
-	?>
+$sql="update tourpackagestbl set PackageName='$pname',PackageType='$ptype',PackageLocation='$plocation',
+PackagePrice='$pprice',PackageFetures='$pfeatures',PackageDetails='$pdetails' where PackageId=$pid ";
+
+$query_result = mysqli_query($con, $sql) or die(mysqli_error($con));
+
+
+}?>
+
+
 <!DOCTYPE HTML>
 <html>
 <head>
-<title>TMS | Admin Package Creation</title>
+<title>TP | Admin Package Updation</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<!--js -->
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/scripts.js"></script>
+<!-- Bootstrap Core JavaScript -->
+   <script src="js/bootstrap.min.js"></script>
+<!-- /Bootstrap Core JavaScript -->	  
 <meta name="keywords" content="Pooled Responsive web template, Bootstrap Web Templates, Flat Web Templates, Android Compatible web template, 
 Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyEricsson, Motorola web design" />
 <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
@@ -65,132 +61,118 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
     -webkit-box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
     box-shadow: 0 1px 1px 0 rgba(0,0,0,.1);
 }
-		</style>
+</style>
 
-</head> 
+</head>
+
 <body>
-   <div class="page-container">
+
+<div class="page-container">
    <!--/content-inner-->
-<div class="left-content">
-	   <div class="mother-grid-inner">
-              <!--header start here-->
-<?php include('includes/header.php');?>
+
+	<div class="left-content">
+
+   		<div class="mother-grid-inner">
+     	<!--header start here-->
+     	<?php include('includes/header.php');?>
 							
-				     <div class="clearfix"> </div>	
-				</div>
-<!--heder end here-->
-	<ol class="breadcrumb">
-                <li class="breadcrumb-item"><a href="index.html">Home</a><i class="fa fa-angle-right"></i>Update Tour Package </li>
-            </ol>
+		</div>
+		<!--heder end here-->
+		<ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="dashboard.php">Home</a>
+            <i class="fa fa-angle-right"></i>Update Tour Package </li>
+     	</ol>
+
 		<!--grid-->
- 	<div class="grid-form">
- 
-<!---->
-  <div class="grid-form1">
-  	       <h3>Update Package</h3>
-  	        	  <?php if($error){?><div class="errorWrap"><strong>ERROR</strong>:<?php echo htmlentities($error); ?> </div><?php } 
-				else if($msg){?><div class="succWrap"><strong>SUCCESS</strong>:<?php echo htmlentities($msg); ?> </div><?php }?>
-  	         <div class="tab-content">
-						<div class="tab-pane active" id="horizontal-form">
-						
+		<div class="grid-form">
+		<div class="grid-form1">
+    		<h3>Update Package</h3>
+
+<div class="tab-content">	
+<div class="tab-content">
+<div class="tab-pane active" id="horizontal-form">						
 <?php 
 $pid=intval($_GET['pid']);
-$sql = "SELECT * from TblTourPackages where PackageId=:pid";
-$query = $dbh -> prepare($sql);
-$query -> bindParam(':pid', $pid, PDO::PARAM_STR);
-$query->execute();
-$results=$query->fetchAll(PDO::FETCH_OBJ);
+$select_query = "SELECT * from tourpackagestbl where PackageId=$pid ";
+$select_query_result = mysqli_query($con, $select_query) or die(mysqli_error($con));
 $cnt=1;
-if($query->rowCount() > 0)
-{
-foreach($results as $result)
-{	?>
+$row = mysqli_fetch_assoc($select_query_result);
+while($row > 0){
+?>
 
-							<form class="form-horizontal" name="package" method="post" enctype="multipart/form-data">
-								<div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Package Name</label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control1" name="packagename" id="packagename" placeholder="Create Package" value="<?php echo htmlentities($result->PackageName);?>" required>
-									</div>
-								</div>
-<div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Package Type</label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control1" name="packagetype" id="packagetype" placeholder=" Package Type eg- Family Package / Couple Package" value="<?php echo htmlentities($result->PackageType);?>" required>
-									</div>
-								</div>
-
-<div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Package Location</label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control1" name="packagelocation" id="packagelocation" placeholder=" Package Location" value="<?php echo htmlentities($result->PackageLocation);?>" required>
-									</div>
-								</div>
-
-<div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Package Price in USD</label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control1" name="packageprice" id="packageprice" placeholder=" Package Price is USD" value="<?php echo htmlentities($result->PackagePrice);?>" required>
-									</div>
-								</div>
-
-<div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Package Features</label>
-									<div class="col-sm-8">
-										<input type="text" class="form-control1" name="packagefeatures" id="packagefeatures" placeholder="Package Features Eg-free Pickup-drop facility" value="<?php echo htmlentities($result->PackageFetures);?>" required>
-									</div>
-								</div>		
-
-
-<div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Package Details</label>
-									<div class="col-sm-8">
-										<textarea class="form-control" rows="5" cols="50" name="packagedetails" id="packagedetails" placeholder="Package Details" required><?php echo htmlentities($result->PackageDetails);?></textarea> 
-									</div>
-								</div>															
-<div class="form-group">
-<label for="focusedinput" class="col-sm-2 control-label">Package Image</label>
-<div class="col-sm-8">
-<img src="pacakgeimages/<?php echo htmlentities($result->PackageImage);?>" width="200">&nbsp;&nbsp;&nbsp;<a href="change-image.php?imgid=<?php echo htmlentities($result->PackageId);?>">Change Image</a>
-</div>
-</div>
-
-<div class="form-group">
-									<label for="focusedinput" class="col-sm-2 control-label">Last Updation Date</label>
-									<div class="col-sm-8">
-<?php echo htmlentities($result->UpdationDate);?>
-									</div>
-								</div>		
-								<?php }} ?>
-
-								<div class="row">
-			<div class="col-sm-8 col-sm-offset-2">
-				<button type="submit" name="submit" class="btn-primary btn">Update</button>
+<form class="form-horizontal" name="package" method="post" enctype="multipart/form-data">
+	<div class="form-group">
+		<label for="focusedinput" class="col-sm-2 control-label">Package Name</label>
+		<div class="col-sm-8">
+		<input type="text" class="form-control1" name="packagename" id="packagename" placeholder="Create Package" value="<?php echo $row['PackageName'];?>" required>
+		</div>
+	</div>
+	<div class="form-group">
+			<label for="focusedinput" class="col-sm-2 control-label">Package Type</label>
+			<div class="col-sm-8">
+			<input type="text" class="form-control1" name="packagetype" id="packagetype" placeholder=" Package Type eg- Family Package / Couple Package" value="<?php echo $row['PackageType'];?>" required>
 			</div>
 		</div>
-						
-					
-						
-						
-						
-					</div>
-					
-					</form>
+		<div class="form-group">
+			<label for="focusedinput" class="col-sm-2 control-label">Package Location</label>
+			<div class="col-sm-8">
+			<input type="text" class="form-control1" name="packagelocation" id="packagelocation" placeholder=" Package Location" value="<?php echo $row['PackageLocation'];?>" required>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="focusedinput" class="col-sm-2 control-label">Package Price is INR</label>
+			<div class="col-sm-8">
+			<input type="text" class="form-control1" name="packageprice" id="packageprice" placeholder=" Package Price is INR" value="<?php echo $row['PackagePrice'];?>" required>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="focusedinput" class="col-sm-2 control-label">Package Features</label>
+			<div class="col-sm-8">
+			<input type="text" class="form-control1" name="packagefeatures" id="packagefeatures" placeholder="Package Features Eg-free Pickup-drop facility" value="<?php echo $row['PackageFetures'];?>" required>
+			</div>
+		</div>		
+		<div class="form-group">
+			<label for="focusedinput" class="col-sm-2 control-label">Package Details</label>
+			<div class="col-sm-8">
+			<textarea class="form-control" rows="5" cols="50" name="packagedetails" id="packagedetails" placeholder="Package Details" required><?php echo $row['PackageDetails'];?></textarea> 
+			</div>
+		</div>															
+		<div class="form-group">
+			<label for="focusedinput" class="col-sm-2 control-label">Package Image</label>
+			<div class="col-sm-8">
+			<img src="pacakgeimages/<?php echo $row['packageImage'];?>" id="packageimage"width="200">&nbsp;&nbsp;&nbsp;
+			<a href="change-image.php?imgid=<?php echo $row['PackageId'];?>"> Change Image</a>
+			</div>
+		</div>
+		<div class="form-group">
+			<label for="focusedinput" class="col-sm-2 control-label">Last Updation Date</label>
+			<div class="col-sm-8">
+			<?php echo $row['UpdationDate'];?>
+			</div>
+		</div>		
+	
 
-     
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+		<button type="submit" name="submit" class="btn-primary btn">Update</button>
+		</div>
+	</div>
+	
+ </div>	
+ <?php $row =-1 ; } ?>
+
+</form>
+    
       
 
-      
-      <div class="panel-footer">
-		
 	 </div>
-    </form>
-  </div>
- 	</div>
- 	<!--//grid-->
+		</div> 
 
+
+
+    
 <!-- script-for sticky-nav -->
-		<script>
+<script>
 		$(document).ready(function() {
 			 var navoffeset=$(".header-main").offset().top;
 			 $(window).scroll(function(){
@@ -249,4 +231,7 @@ foreach($results as $result)
 
 </body>
 </html>
-<?php } ?>
+
+
+
+
